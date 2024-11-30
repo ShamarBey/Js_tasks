9a4832335d4ad656060a10b4a8fe44480549e20d
Alim <rizaev.alim06@mail.ru>
2024-11-30 22:02:52 +0300

    Задача 49

--- The basics/1-41/42-52/script_42-52.js
+++ The basics/1-41/42-52/script_42-52.js
@@ -103,3 +103,14 @@
 // let a48 = 5;
 // let b48 = 45;
 // alert(String(a48) + String(b48)); 
+
+// Задание 49 №1
+// let a49_1 = 545;
+// alert(String(a49_1).length);
+
+// Задание 49 №2
+// let a49_2 = 55; 
+// let b49_2 = 245;   
+// let sum49_2 = String(a49_2).length + String(b49_2).length;
+// alert(sum49_2);
+
