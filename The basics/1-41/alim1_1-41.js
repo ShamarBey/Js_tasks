216995f53fe9bc3332928f67c562f64578143b55
Alim <rizaev.alim06@mail.ru>
2024-11-30 21:52:21 +0300

    Задача 29

--- The basics/1-41/alim1_1-41.js
+++ The basics/1-41/alim1_1-41.js
@@ -102,3 +102,16 @@ dfsdf */
 // let surname28_1 = "Rizaev";
 // alert(surname28_1);
 
+// Задание 29 №1
+// let str29 = "!!!";
+// alert(str29);
+
+// Задание 29 №2
+// let str29_1 = "java";
+// let str29_2 = "script";
+// alert(str29_1 + str29_2);
+
+// Задание 29 №3
+// let str29_3 = "hello";
+// let str29_4 = "world";
+// alert(str29_3 + ' ' + str29_4);
