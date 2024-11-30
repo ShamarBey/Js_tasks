281ec1bcc2ca35529c6b4802c75277c9c1f7acf0
Alim <rizaev.alim06@mail.ru>
2024-11-30 21:50:24 +0300

    Задача 18

--- The basics/1-41/alim1_1-41.js
+++ The basics/1-41/alim1_1-41.js
@@ -37,3 +37,35 @@ dfsdf */
 // 17
 // let a = 1 + 2 + 3;
 // alert(a);
+
+// Задание 18 №1
+// let a1 = 10;
+// let b1 = 2;
+
+// alert(a1+b1)
+// alert(a1-b1)
+// alert(a1*b1)
+// alert(a1/b1)
+
+// Задание 18 №2
+// let c = 10;
+// let d = 5;
+// let result= c + d;
+
+// alert(result)
+
+// Задание 18 №3
+// let a3 = 1;
+// let b3 = 2;
+// let c3 = 3;
+
+// alert(a3+b3+c3)
+
+// Задание 18 №4
+// let a4 = 10;
+// let b4 = 5;
+// let c4 = a4 - b4;
+// let d4 = 7;
+// let result4 = c4 + d4;
+
+// alert(result4);
