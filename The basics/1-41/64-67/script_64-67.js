b19a9f4ef2c39ae02a1b1dcc7338ae70b9720eee
Alim <rizaev.alim06@mail.ru>
2024-11-30 22:18:26 +0300

    Задача 65

--- The basics/1-41/64-67/script_64-67.js
+++ The basics/1-41/64-67/script_64-67.js
@@ -1,2 +1,83 @@
 // Задание 64 №1
 // document.write('Задание 64 №1<br>');
+
+// Задание 64 №2
+// document.write('<i>Задание 64 №2</i> <br>');
+
+// Задание 64 №3
+// let str64 = 'Задание 64 №3';
+// document.write('<i>' + str64 + '</i> <br>')
+
+// Задание 64 №4
+// document.write('1<br>');
+// document.write('2<br>');
+// document.write('3<br>');
+// document.write('4<br>');
+// document.write('5<br>');
+
+// Задание 65 №1
+// let num65_1 = 1;
+// let num65_2 = 2;
+// console.log('сумма: ' + (num65_1 + num65_2));
+
+// Задание 65 №2
+// let a65_2 = 1;
+// let b65_2 = 2;
+// console.log(a65_2 + b65_2);
+
+// Задание 65 №3
+// let num65_3 = '123';
+// let sum65_3 = Number(num65_3[0]) + Number(num65_3[1]) + Number(num65_3[2]);
+// console.log(sum65_3);
+
+// Задание 65 №4
+// let num65_4 = String(123);
+// console.log((num65_4[0]));
+
+// Задание 65 №5
+// let c65_5 = 0;
+// console.log(++c65_5);
+
+// Задание 65 №6
+// let num65_6 = String(123);
+// console.log(num65_6.length);
+
+// Задание 65 №7
+// let k65_7 = 24 * 60 * 60;
+// console.log(k65_7);
+
+
+// Задание 65 №8
+// let num65_8 = 123;
+// let str65_8 = String(num65_8);
+// console.log(str65_8.length);
+
+
+// Задание 65 №9
+// let num65_9 = 123;
+// let str65_9 = String(num65_9);
+// console.log(str65_9[str65_9.length - 1]);
+
+// Задание 65 №10
+// let num65_10 = 123;
+// let str65_10 = String(num65_10);
+// console.log(str65_10.length);
+
+// Задание 65 №11
+// let num65_11 = 123;
+// let str65_11 = String(num65_11);
+// console.log(str65_11[str65_11.length - 1]);
+
+
+// Задание 65 №12
+// let a65_12 = '123';
+// let b65_12 = '456';
+// let s65_12 = Number(a65_12) + Number(b65_12);
+// console.log(s65_12);
+
+// Задание 65 №13
+// let aaa = 1;
+// let bbb = 2;
+// let ccc = 3;
+// console.log(aaa + bbb + ccc);
+
