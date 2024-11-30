7b0e8910f0357b06bdfac3799a6941b881ff7589
Alim <rizaev.alim06@mail.ru>
2024-11-30 22:10:40 +0300

    Задача 54

---
+++ The basics/1-41/53-63/script_53-63.js
@@ -0,0 +1,12 @@
+// Задание 54 №1
+// let str54_1 = 'serverosmanov';
+// alert(str54_1[str54_1.length - 1]);
+
+// Задание 54 №2
+// let str54_2 = 'serverosmanov';
+// alert(str54_2[str54_2.length - 2]);
+
+// Задание 54 №3
+// let str54_3 = 'serverosmanov';
+// alert(str54_3[str54_3.length - 3]);
+
