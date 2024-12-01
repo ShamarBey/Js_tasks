7ba1ccfcaf3af9e11492cc9b39eed7b5a6aa52b9
Alim <rizaev.alim06@mail.ru>
2024-12-01 11:58:30 +0300

    Задача 77

--- Arrays/script_68-79.js
+++ Arrays/script_68-79.js
@@ -89,3 +89,14 @@
 // arr76_2.push(5);
 // console.log(arr76_2)
 
+// Задание 77 №1
+// let arr77_1 = ['a', 'b', 'c'];
+// let key77_1 = 2;
+// console.log(arr77_1[key77_1]);
+
+// Задание 77 №2
+// let arr77_2 = [1, 2, 3, 4, 5];
+// let key77_2 = 1;
+// let key77_3 = 2;
+// console.log(arr77_2[key77_2] + arr77_2[key77_3]);
+
