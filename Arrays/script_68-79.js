61692eec7e67850e5322ec663535074ae946c428
Alim <rizaev.alim06@mail.ru>
2024-12-01 11:56:16 +0300

    Задача 68

---
+++ Arrays/script_68-79.js
@@ -0,0 +1,9 @@
+// Задание 68 №1
+// let arr68_1 = [1, 2, 3, 4, 5]; 
+// alert(arr68_1);
+// console.log(arr68_1);
+
+// Задание 68 №2
+// let arr68_2 = ["tiana"]; 
+// alert(arr68_2);
+// console.log(arr68_2);
