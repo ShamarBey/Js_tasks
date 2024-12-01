9c4c66b0d641e331a248bcc0dac8e32b048893f6
Alim <rizaev.alim06@mail.ru>
2024-12-01 13:43:57 +0300

    Задача 82

--- Objects/script_80-103.js
+++ Objects/script_80-103.js
@@ -5,3 +5,7 @@ console.log(obj80[1], obj80[2], obj80[3], obj80[4], obj80[5], obj80[6], obj80[6]
 // Задание 81 №1
 let obj81 = {1: 'January', 2: 'February', 3: 'March', 4: "April", 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'};
 console.log(obj81);
+
+// Задание 82 №1
+let user82={name: "Catherine" , surname: "Grebeneva", patronymic: "Andreevna"};
+console.log(user82["surname"]+" "+user82["name"]+' '+user82["patronymic"]);
