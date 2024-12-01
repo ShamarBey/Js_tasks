57a2d3d57bd50b8c667f338e02aa85730435d7ca
Alim <rizaev.alim06@mail.ru>
2024-12-01 20:55:14 +0300

    Задача 100

--- Objects/script_80-103.js
+++ Objects/script_80-103.js
@@ -194,3 +194,23 @@ console.log('x' in obj95);
 // 5. undefined
 // 6. symbol (ES6)
 // 7. bigint (ES11)
+
+// Задание 100 №1
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr1[0] = 'a';
+// console.log(arr2);
+
+// Задание 100 №2
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr1[0] = 'a';
+// arr2[1] = 'b';
+// console.log(arr1);
+
+// Задание 100 №3
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr1[0] = 'a';
+// arr2[0] = 'b';
+// console.log(arr2);
