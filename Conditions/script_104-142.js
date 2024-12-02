2fd64f671b4755bdd76f4080a42c4a0b61f9bed0
Alim <rizaev.alim06@mail.ru>
2024-12-02 22:47:01 +0300

    Задание 121

--- Conditions/script_104-142.js
+++ Conditions/script_104-142.js
@@ -397,3 +397,27 @@
 // } else {
 // 	console.log('---');
 // }
+
+// Задание 121 №1
+// let test121_1 = true;
+// if (!test121_1) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 121 №2
+// let test121_2 = true;
+// if (!test121_2) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+
+// Задание 121 №3
+// let test121_3 = true;
+// if (test121_3) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
