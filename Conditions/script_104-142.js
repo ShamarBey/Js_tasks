17a80d241311d8d89a9ee46df2a38e0bb48e57d6
Alim <rizaev.alim06@mail.ru>
2024-12-02 22:45:09 +0300

    Задание 114

--- Conditions/script_104-142.js
+++ Conditions/script_104-142.js
@@ -195,3 +195,73 @@
 // } else {
 // 	console.log('---');
 // }
+
+// Задание 114 №1
+// // let num1 = -10;
+// // let num2 = -10;
+// // if (num1 >= 0 && num2 >= 0) {
+// //   console.log('+++');
+// // } else {
+// //   console.log('---');
+// // }
+ 
+// Задание 114 №2
+// // let num1 = 0;
+// // let num2 = 0;
+// // if (num1 >= 0 && num2 >= 0) {
+// //   console.log('+++');
+// // } else {
+// //   console.log('---');
+// // }
+        
+// Задание 114 №3
+// // let num1 = 0;
+// // let num2 = 5;
+// // if (num1 >= 0 && num2 >= 0) {
+// //   console.log('+++');
+// // } else {
+// //   console.log('---');
+// // }
+
+// Задание 114 №4
+// // let num1 = 5;
+// // let num2 = 5;
+// // if (num1 >= 0 && num2 >= 0) {
+// //   console.log('+++');
+// // } else {
+// //   console.log('---');
+// // }
+
+// Задание 114 №5
+// // let num1 = -5;
+// // let num2 = 15;
+// // if (num1 >= 0 && num2 >= 0) {
+// //   console.log('+++');
+// // } else {
+// //   console.log('---');
+// // }
+
+// Задание 114 №6
+// // let num1 = -5;
+// // let num2 = 15;
+// // if (num1 >= 0 && num2 >= 0) {
+// //   console.log('+++');
+// // } else {
+// //   console.log('---');
+// // }
+
+// Задание 114 №7
+// // let num = 2;
+// // if (num == 0 || num == 1) {
+// //   console.log('+++');
+// // } else {
+// //   console.log('---');
+// // }
+
+// Задание 114 №8
+// // let num = 2;
+// // if (num == 0 || num == 1 || num == 2) {
+// //   console.log('+++');
+// // } else {
+// //   console.log('---');
+// // }
