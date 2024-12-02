d337df5cca97fdef0c6def8089472ca34e5388e5
Alim <rizaev.alim06@mail.ru>
2024-12-02 22:44:30 +0300

    Задание 112

--- Conditions/script_104-142.js
+++ Conditions/script_104-142.js
@@ -125,3 +125,48 @@
 // // } else {
 // // 	console.log('---');
 // // }
+
+// Задание 112 №1
+// // let test1 = '3';
+// // let test2 = '3';
+// // if (test1 != test2) {
+// // 	console.log('+++');
+// // } else {
+// // 	console.log('---');
+// // }
+
+// Задание 112 №2
+// // let test1 = '3';
+// // let test2 = '3';		
+// // if (test1 !== test2) {
+// // 	console.log('+++');
+// // } else {
+// // 	console.log('---');
+// // }
+
+// Задание 112 №3
+// // let test1 = 3;
+// // let test2 = '3';	
+// // if (test1 != test2) {
+// // 	console.log('+++');
+// // } else {
+// // 	console.log('---');
+// // }
+
+// Задание 112 №4
+// // let test1 = 3;
+// // let test2 = '3';		
+// // if (test1 !== test2) {
+// // 	console.log('+++');
+// // } else {
+// // 	console.log('---');
+// // }
+
+// Задание 112 №5
+// // let test1 = 3;
+// // let test2 = 2;	
+// // if (test1 !== test2) {
+// // 	console.log('+++');
+// // } else {
+// // 	console.log('---');
+// // }
