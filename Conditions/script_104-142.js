f3f75a69d0e6c1d9ba6b8a4cc95fc3f5c721d3c3
Alim <rizaev.alim06@mail.ru>
2024-12-02 22:38:51 +0300

    Задание 105

---
+++ Conditions/script_104-142.js
@@ -0,0 +1,31 @@
+// Задание 105 №1
+// let test105_1 = 20;
+// if (test105_1 > 10) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 105 №2
+// let test105_2 = 5;
+// if (test105_2 < 10) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 105 №3
+// let test105_3 = 10;
+// if (test105_3 >= 10) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
+
+// Задание 105 №4
+// let test105_4 = 2;
+// if (test105_4 <= 10) {
+// 	console.log('+++'); 
+// } else {
+// 	console.log('---');
+// }
