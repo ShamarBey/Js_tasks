1c8027cd9506865a568508e36a8017436fdbd477
Alim <rizaev.alim06@mail.ru>
2024-12-08 22:48:38 +0300

    Задача 145

--- Cycles/script_143-167.js
+++ Cycles/script_143-167.js
@@ -4,3 +4,19 @@ let arr144 = ['a', 'b', 'c', 'd', 'e'];
 for (let elem144 of arr144) {
 	console.log(elem144);
 }
+
+// Задание 145 №1
+console.log('Результат задания 145 №1');
+let obj145_1 = {x: 1, y: 2, z: 3};
+console.log('Ключи объекта:'); 
+for (let key145_1 in obj145_1) {
+	console.log(key145_1); 
+}
+
+// Задание 145 №2
+console.log('Результат задания 145 №2');
+let obj145_2 = {x: 1, y: 2, z: 3};
+console.log('Элементы объекта:'); 
+for (let key145_2 in obj145_2) {
+	console.log(obj145_2[key145_2]); 
+}
