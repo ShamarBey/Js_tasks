9675b654642107aea63149d7932d0bc67fd8fa82
Alim <rizaev.alim06@mail.ru>
2024-12-10 21:24:52 +0300

    Задача 200

---
+++ User-defined functions/script_200-214.js
@@ -0,0 +1,16 @@
+console.log('Результат задания 200 №1:');
+function func() {
+	console.log('Katushka');
+}
+func();
+
+// Задание 200 №2
+console.log('Результат задания 200 №2:');
+function res200_2() {
+	let sum200_2 = 0;
+	for(let i = 1; i <= 100; i++){		
+		sum200_2 += i;
+	}
+  console.log(sum200_2);
+}
+res200_2();
