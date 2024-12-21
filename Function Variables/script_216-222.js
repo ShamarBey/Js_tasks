a2be04e697dccb4b5dae06f975b56376d45d4494
Alim <rizaev.alim06@mail.ru>
2024-12-21 15:23:20 +0300

    Задача 222

---
+++ Function Variables/script_216-222.js
@@ -0,0 +1,225 @@
+// Задание 216 №1
+let num216_1 = 1;
+function func() {
+    console.log(num216_1);
+}
+func();
+
+// Задание 216 №2
+let num216_2 = 1;
+function func() {
+    console.log(num216_2);
+}
+num216_2 = 2;
+func();  
+
+// Задание 216 №3
+function func() {
+    console.log(num216_3);
+}
+let num216_3;
+num216_3 = 1;
+func();
+num216_3 = 2;
+func();
+
+
+// Задание 217 №1
+function func() {
+	let num217_1 = 5;
+	return num217_1;
+}
+console.log(num217_1);
+
+// Задание 217 №2
+function func() {
+	let num217_2 = 5;
+	return num217_2;
+}
+console.log(func());
+
+
+// Задание 218 №1
+let num218_1 = 1;
+function func() {
+	num218_1 = 2;
+}
+func();
+console.log(num218_1);
+
+// Задание 218 №2
+let num218_2 = 1;
+function func() {
+	let num218_2 = 2;
+}
+func();
+console.log(num218_2);
+
+
+// Задание 219 №1
+let num219_1 = 1;
+function func() {
+  num219_1 = 2;
+}
+func();
+console.log(num219_1);
+
+// Задание 219 №2
+let num219_2 = 1;
+function func() {
+  num219_2 = 2;
+}
+console.log(num219_2);
+
+// Задание 219 №3
+let num219_3 = 1;
+function func() {
+  let num219_3 = 2;
+}
+func();
+console.log(num219_3); 
+
+// Задание 219 №4
+let num219_4 = 1;
+function func() {
+  let num219_4 = 2;
+}
+console.log(num219_4);
+func(); 
+
+// Задание 219 №5
+let num219_5 = 1;
+function func() {
+  num219_5 = 2;
+}
+console.log(num219_5);
+func(); 
+
+// Задание 219 №6
+let num219_6 = 1;
+function func() {
+  num219_6++;
+}
+func();
+func();
+func();
+console.log(num219_6);
+
+// Задание 219 №7
+function func() {
+  num219_7 = 2;
+}
+let num219_7 = 1;
+console.log(num219_7);
+func();
+
+// Задание 219 №8
+function func() {
+  num219_8 = 2;
+}
+let num219_8 = 1;
+func();
+console.log(num219_8);
+
+
+// Задание 220 №1
+function func(localNum) {
+    console.log(localNum);
+  }
+  func(1);
+  
+  // Задание 220 №2
+  function func(localNum) {
+    console.log(localNum);
+  }
+  let num220_2 = 1;
+  func(num220_2);
+  
+  // Задание 220 №3
+  function func(localNum) {
+    console.log(localNum);
+  }
+  let num220_3 = 1;
+  func(num220_3);
+  num220_3 = 2;
+  
+  // Задание 220 №4
+  let num220_4 = 1;
+  function func(localNum) {
+    console.log(localNum);
+  }
+  num220_4 = 2;
+  func(num220_4);
+  
+  // Задание 220 №5
+  function func(localNum) {
+    localNum = 2;
+  }
+  let num220_5 = 1;
+  func(num220_5);
+  console.log(num220_5);
+  
+  // Задание 220 №6
+  function func(localNum) {
+    num220_6 = 2;
+  }
+  let num220_6 = 1;
+  func(num220_6);
+  console.log(num220_6);
+  
+  // Задание 220 №7
+  function func(localNum) {
+    let num220_7 = 2;
+  }
+  let num220_7 = 1;
+  func(num220_7);
+  console.log(num220_7);
+  
+
+  // Задание 221 №1
+function func(num221_1) {
+    num221_1 = 2;
+  }
+  let num221_1 = 1;
+  func(num221_1);
+  console.log(num221_1);
+  
+  // Зание 221 №2
+  function func() {
+    num221_2 = 2;
+  }
+  let num221_2 = 1;
+  func();
+  console.log(num221_2);
+  
+  // Зание 221 №3
+  function func() {
+    let num221_3 = 2;
+  }
+  let num221_3 = 1;
+  func();
+  console.log(num221_3);
+
+  // Задание 221 №1
+function func(num221_1) {
+    num221_1 = 2;
+  }
+  let num221_1 = 1;
+  func(num221_1);
+  console.log(num221_1);
+  
+  // Зание 221 №2
+  function func() {
+    num221_2 = 2;
+  }
+  let num221_2 = 1;
+  func();
+  console.log(num221_2);
+  
+  // Зание 221 №3
+  function func() {
+    let num221_3 = 2;
+  }
+  let num221_3 = 1;
+  func();
+  console.log(num221_3);
